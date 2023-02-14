04d826b6e420155ba0ec5b5315056b099b4ed521
YaraBertazzi <87441514+YaraBertazzi@users.noreply.github.com>
2023-02-14 16:29:11 -0300

    Atualizado

--- jogo.py
+++ jogo.py
@@ -60,83 +60,90 @@ O   |
 class Hangman:
 
 	# Método Construtor
-	def __init__(self, word):
-		self.word = word
-		self.missed_letters = []
-		self.guessed_letters = []
+	def __init__(self, palavra):
+		self.palavra = palavra #palavra
+		self.letras_perdidas = [] # letras_perdidas
+		self.letras_adivinhadas = [] #letras_adivinhadas
 		
 	# Método para adivinhar a letra
-	def guess(self, letter):
-		if letter in self.word and letter not in self.guessed_letters:
-			self.guessed_letters.append(letter)
-		elif letter not in self.word and letter not in self.missed_letters:
-			self.missed_letters.append(letter)
+	def adivinhar(self, letra): #adivinhar   #letters = letra
+
+		#se a letra estiver em (palavra) self.word e a letra não estiver (sido adivinhada) self.letras_adivinhadas:# acrescenta em palavras adininhadas
+		if letra in self.palavra and letra not in self.letras_adivinhadas:
+			self.letras_adivinhadas.append(letra)
+
+		#ou se letra  não estiver em self.word e letra não estiver em (letras perdidas) self.letras_perdidas: # acrescenta em letras perdidas
+		elif letra not in self.palavra and letra not in self.letras_perdidas:
+			self.letras_perdidas.append(letra)
 		else:
 			return False
 		return True
 		
 	# Método para verificar se o jogo terminou
-	def hangman_over(self):
-		return self.hangman_won() or (len(self.missed_letters) == 6)
+	def carrasco_over(self):
+		return self.carrasco_won() or (len(self.letras_perdidas) == 6)
 		
 	# Método para verificar se o jogador venceu
-	def hangman_won(self):
-		if '_' not in self.hide_word():
+	def carrasco_won(self):
+		# # se '_' não estiver em self.palavra_oculta():
+		if '_' not in self.palavra_oculta():
 			return True
 		return False
 		
 	# Método para não mostrar a letra no board
-	def hide_word(self):
+	def palavra_oculta(self):
 		rtn = ''
-		for letter in self.word:
-			if letter not in self.guessed_letters:
+		for letra in self.palavra:
+			if letra not in self.letras_adivinhadas:
 				rtn += '_'
 			else:
-				rtn += letter
+				rtn += letra
 		return rtn
 		
 	# Método para checar o status do game e imprimir o board na tela
 	def print_game_status(self):
-		print (board[len(self.missed_letters)])
-		print ('\nPalavra: ' + self.hide_word())
+		print (board[len(self.letras_perdidas)])
+		print ('\nPalavra: ' + self.palavra_oculta())
 		print ('\nLetras erradas: ',) 
-		for letter in self.missed_letters:
-			print (letter,) 
+		for letra in self.letras_perdidas:
+			print (letra, )
 		print ()
 		print ('Letras corretas: ',)
-		for letter in self.guessed_letters:
-			print (letter,)
+		for letra in self.letras_adivinhadas:
+			print (letra, )
 		print ()
 
 # Método para ler uma palavra de forma aleatória do banco de palavras
-def rand_word():
-        with open("arquivo.txt", "rt") as f:
-                bank = f.readlines()
-        return bank[random.randint(0,len(bank))].strip()
+def rand_palavra():
+	with open("arquivo.txt", "rt") as f:
+		bank = f.readlines()
+	return bank[random.randint(0, len(bank))].strip()
 
 # Método Main - Execução do Programa
 def main():
 
 	# Objeto
-	game = Hangman(rand_word())
+	game = Hangman(rand_palavra())
 
-	# Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
-	while not game.hangman_over():
+	# Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter*
+	while not game.carrasco_over():
 		game.print_game_status()
 		user_input = input('\nDigite uma letra: ')
-		game.guess(user_input)
+		game.adivinhar(user_input)
 
 	# Verifica o status do jogo
 	game.print_game_status()	
 
 	# De acordo com o status, imprime mensagem na tela para o usuário
-	if game.hangman_won():
+	if game.carrasco_won():
 		print ('\nParabéns! Você venceu!!')
 	else:
 		print ('\nGame over! Você perdeu.')
-		print ('A palavra era ' + game.word)
+		print ('A palavra era ' + game.palavra)
 		
 	print ('\nFoi bom jogar com você! Agora vá estudar!\n')
+
+
 
 # Executa o programa		
 if __name__ == "__main__":
